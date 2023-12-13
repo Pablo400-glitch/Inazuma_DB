7a3e462df8db40b9a28c2c34d86e0d0daeebdd44
Pablo400-glitch <alu0101311135@ull.edu.es>
2023-12-13 20:19:03 +0000

    Add JSON API endpoints for players, teams, stadiums, special moves, matches, and trainings

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 ﻿import os
 import psycopg2
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -25,7 +25,30 @@ def Players():
     players = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('players.html', players=players)
+
+    players_json = []
+    for player in players:
+        player_dict = {
+            'Nombre': player[1],
+            'Apellidos': player[2],
+            'Edad': player[3],
+            'Posicion': player[4],
+            'Nacionalidad': player[5],
+            'Nombre_equipo': player[14],
+            'Atributos': {
+                'Fuerza': player[6],
+                'Velocidad': player[7],
+                'Resistencia': player[8],
+                'Tecnica': player[9],
+                'Porteria': player[10],
+                'Regate': player[11],
+                'Tiro': player[12],
+                'Aguante': player[13]
+            }
+        }
+        players_json.append(player_dict)
+
+    return jsonify(players_json)
 
 @app.route('/teams/')
 def Teams():
@@ -43,7 +66,20 @@ def Teams():
     teams = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('teams.html', teams=teams)
+    
+    teams_json = []
+    for team in teams:
+        team_dict = {
+            'Nombre': team[0],
+            'Pais': team[1],
+            'Victorias': team[2],
+            'Goles a favor': team[3],
+            'Goles en contra': team[4],
+            'Jugadores': team[5]
+        }
+        teams_json.append(team_dict)
+
+    return jsonify(teams_json)
 
 @app.route('/stadiums/')
 def Stadiums():
@@ -53,7 +89,17 @@ def Stadiums():
     stadiums = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('stadium.html', stadiums=stadiums)
+    
+    stadiums_json = []
+    for stadium in stadiums:
+        stadium_dict = {
+            'Nombre': stadium[1],
+            'Tipo de Cesped': stadium[2],
+            'Tipo de Estadio': stadium[3]
+        }
+        stadiums_json.append(stadium_dict)
+
+    return jsonify(stadiums_json)
 
 @app.route('/Special_Moves/')
 def Special_Moves():
@@ -63,4 +109,69 @@ def Special_Moves():
     special_moves = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('special_moves.html', special_moves=special_moves)
+    
+    special_moves_json = []
+    for special_move in special_moves:
+        special_move_dict = {
+            'Nombre': special_move[1],
+            'Elemento': special_move[2],
+            'Tipo': special_move[3],
+            'Numero de Jugadores con supertecnica': special_move[4]
+        }
+        special_moves_json.append(special_move_dict)
+    
+    return jsonify(special_moves_json)
+
+@app.route('/Matches/')
+def Matches():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT P.id_partido, E1.nombre AS equipo_local, '
+                'E2.nombre AS equipo_visitante, '
+                'ES.nombre AS nombre_estadio, '
+                'P.goles_local, P.goles_visitante, P.fecha '
+                'FROM PARTIDO P '
+                'JOIN EQUIPO E1 ON P.id_equipo_local = E1.id_equipo '
+                'JOIN EQUIPO E2 ON P.id_equipo_visitante = E2.id_equipo '
+                'JOIN ESTADIO ES ON P.id_estadio = ES.id_estadio;')
+    matches = cur.fetchall()
+    cur.close()
+    conn.close()
+    
+    matches_json = []
+    for match in matches:
+        match_dict = {
+            'Equipo Local': match[1],
+            'Equipo Visitante': match[2],
+            'Nombre Estadio': match[3],
+            'Goles Equipo Local': match[4],
+            'Goles Equipo Visitante': match[5]
+        }
+        matches_json.append(match_dict)
+    
+    return jsonify(matches_json)
+    
+@app.route('/Trainings/')
+def Trainings():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT EN.fecha,'
+                'E.nombre AS nombre_equipo, '
+                'EN.lugar,EN.tipo '
+                'FROM ENTRENAMIENTO EN '
+                'JOIN EQUIPO E ON EN.id_equipo = E.id_equipo;')
+    trainings = cur.fetchall()
+    cur.close()
+    conn.close()
+    
+    trainings_json = []
+    for training in trainings:
+        training_dict = {
+            'Fecha': training[0],
+            'Nombre Equipo': training[1],
+            'Lugar': training[2],
+            'Tipo': training[3]
+        }
+        trainings_json.append(training_dict)
+    
+    return jsonify(trainings_json)
