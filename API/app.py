0e585ae0d3b5e511995e6507438e695089ddb591
Pablo400-glitch <paisficretpablo@gmail.com>
2023-12-16 12:19:55 +0000

    Added date trigger

--- API/app.py
+++ API/app.py
@@ -50,6 +50,30 @@ def Players():
 
     return jsonify(players_json)
 
+@app.route('/delete_player/<int:id_player>')
+def delete_player(id_player):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar el jugador con el ID proporcionado
+        cur.execute('DELETE FROM jugador WHERE id_jugador = %s;', (id_player,))
+        conn.commit()
+
+        # Verifica si se eliminó algún jugador
+        if cur.rowcount == 0:
+            # No se encontró ningún jugador con el ID proporcionado
+            raise Exception(f'No se encontro un jugador con el ID {id_player}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Player deleted successfully'})
+    
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404
+
 @app.route('/teams/')
 def Teams():
     conn = get_db_connection()
@@ -81,6 +105,30 @@ def Teams():
 
     return jsonify(teams_json)
 
+@app.route('/delete_team/<int:id_team>')
+def delete_team(id_team):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar el equipo con el ID proporcionado
+        cur.execute('DELETE FROM equipo WHERE id_equipo = %s;', (id_team,))
+        conn.commit()
+
+        # Verifica si se eliminó algún equipo
+        if cur.rowcount == 0:
+            # No se encontró ningún equipo con el ID proporcionado
+            raise Exception(f'No se encontro un equipo con el ID {id_team}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Team deleted successfully'})
+    
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404
+
 @app.route('/stadiums/')
 def Stadiums():
     conn = get_db_connection()
@@ -100,6 +148,30 @@ def Stadiums():
         stadiums_json.append(stadium_dict)
 
     return jsonify(stadiums_json)
+
+@app.route('/delete_stadium/<int:id_stadium>')
+def delete_stadium(id_stadium):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar el estadio con el ID proporcionado
+        cur.execute('DELETE FROM estadio WHERE id_estadio = %s;', (id_stadium,))
+        conn.commit()
+
+        # Verifica si se eliminó algún estadio
+        if cur.rowcount == 0:
+            # No se encontró ningún estadio con el ID proporcionado
+            raise Exception(f'No se encontro un estadio con el ID {id_stadium}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Stadium deleted successfully'})
+
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404  # Puedes cambiar el código de estado según sea necesario
 
 @app.route('/Special_Moves/')
 def Special_Moves():
@@ -121,6 +193,30 @@ def Special_Moves():
         special_moves_json.append(special_move_dict)
     
     return jsonify(special_moves_json)
+
+@app.route('/delete_special_move/<int:id_special_move>')
+def delete_special_move(id_special_move):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar la supertecnica con el ID proporcionado
+        cur.execute('DELETE FROM supertecnica WHERE id_supertecnica = %s;', (id_special_move,))
+        conn.commit()
+
+        # Verifica si se eliminó alguna supertecnica
+        if cur.rowcount == 0:
+            # No se encontró ninguna supertecnica con el ID proporcionado
+            raise Exception(f'No se encontro una supertecnica con el ID {id_special_move}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Special Move deleted successfully'})
+
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404
 
 @app.route('/Matches/')
 def Matches():
@@ -150,6 +246,30 @@ def Matches():
         matches_json.append(match_dict)
     
     return jsonify(matches_json)
+
+@app.route('/delete_match/<int:id_match>')
+def delete_match(id_match):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar el partido con el ID proporcionado
+        cur.execute('DELETE FROM partido WHERE id_partido = %s;', (id_match,))
+        conn.commit()
+
+        # Verifica si se eliminó algún partido
+        if cur.rowcount == 0:
+            # No se encontró ningún partido con el ID proporcionado
+            raise Exception(f'No se encontro un partido con el ID {id_match}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Match deleted successfully'})
+
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404
     
 @app.route('/Trainings/')
 def Trainings():
@@ -175,3 +295,27 @@ def Trainings():
         trainings_json.append(training_dict)
     
     return jsonify(trainings_json)
+
+@app.route('/delete_training/<int:id_training>')
+def delete_training(id_training):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Intenta eliminar el entrenamiento con el ID proporcionado
+        cur.execute('DELETE FROM entrenamiento WHERE id_entrenamiento = %s;', (id_training,))
+        conn.commit()
+
+        # Verifica si se eliminó algún entrenamiento
+        if cur.rowcount == 0:
+            # No se encontró ningún entrenamiento con el ID proporcionado
+            raise Exception(f'No se encontro un entrenamiento con el ID {id_training}')
+
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Training deleted successfully'})
+
+    except Exception as e:
+        # Captura cualquier excepción y devuelve un objeto JSON con el mensaje de error
+        return jsonify({'error': str(e)}), 404
